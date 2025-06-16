a7c973d8f488e553fae08dcff7b8b76c0973efbd
Moses-TM <mosesmagar97@gmail.com>
2025-06-16 08:54:05 +0000

    Add playGame function to play the entire game. Refactor playRound function for improved structure and readability

--- script.js
+++ script.js
@@ -1,80 +1,98 @@
-function getComputerChoice(){
-    let choiceNumber = Math.floor(Math.random()*3) + 1
-    return (choiceNumber === 1) ? "rock" : 
-            (choiceNumber === 2) ? "paper" : 
-            "scissors"
+function getComputerChoice() {
+    let choiceNumber = Math.floor(Math.random() * 3) + 1
+    return (choiceNumber === 1) ? "rock" :
+        (choiceNumber === 2) ? "paper" :
+        "scissors"
 }
 
-function getHumanChoice (){
+function getHumanChoice() {
     let humanChoice = prompt("Rock Paper Scissors")
     return humanChoice.toLowerCase()
 }
 
-let humanScore = 0
-let computerScore = 0
-let humanChoice = getHumanChoice()
-let computerChoice = getComputerChoice()
-console.log(humanChoice)
-console.log(computerChoice)
 
-function playRound(humanChoice, computerChoice) {
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
 
-    if(humanChoice === "rock"){
+    function playRound(humanChoice, computerChoice) {
 
-        switch(computerChoice){
-            case "rock":
-                console.log("Draw")
-                break
+        if (humanChoice === "rock") {
 
-            case "paper":
-                console.log("Paper beats Rock. You lost.")
-                computerScore += 1
-                break
-            
-            case "scissors":
-                console.log("Rock beats Scissors. You win.")
-                humanScore += 1
-                break
+            switch (computerChoice) {
+                case "rock":
+                    console.log("Draw")
+                    break
+
+                case "paper":
+                    console.log("Paper beats Rock. You lost.")
+                    computerScore += 1
+                    break
+
+                case "scissors":
+                    console.log("Rock beats Scissors. You win.")
+                    humanScore += 1
+                    break
+            }
+        }
+
+        else if (humanChoice === "paper") {
+            switch (computerChoice) {
+                case "rock":
+                    console.log("Paper beats Rock. You win.")
+                    humanScore += 1
+                    break
+
+                case "paper":
+                    console.log("Draw")
+                    break
+
+                case "scissors":
+                    console.log("Scissors beats Paper. You lost.")
+                    computerScore += 1
+                    break
+            }
+        }
+
+        else {
+            switch (computerChoice) {
+                case "rock":
+                    console.log("Rock beats Scissors. You lost.")
+                    computerScore += 1
+                    break
+
+                case "paper":
+                    console.log("Scissors beats Paper. You win.")
+                    humanScore += 1
+                    break
+
+                case "scissors":
+                    console.log("Draw")
+                    break
+            }
         }
     }
 
-    else if(humanChoice === "paper"){
-        switch(computerChoice){
-            case "rock":
-                console.log("Paper beats Rock. You win.")
-                humanScore += 1
-                break
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice()
+        let computerChoice = getComputerChoice()
+        console.log(humanChoice)
+        console.log(computerChoice)
 
-            case "paper":
-                console.log("Draw")
-                break
-            
-            case "scissors":
-                console.log("Scissors beats Paper. You lost.")
-                computerScore += 1
-                break
-        }
+        playRound(humanChoice, computerChoice)
+        console.log(humanScore)
+        console.log(computerScore)
     }
 
-    else{
-        switch(computerChoice){
-            case "rock":
-                console.log("Rock beats Scissors. You lost.")
-                computerScore += 1
-                break
-
-            case "paper":
-                console.log("Scissors beats Paper. You win.")
-                humanScore += 1
-                break
-            
-            case "scissors":
-                console.log("Draw")
-                break
-        }
+    if (humanScore > computerScore) {
+        console.log(`You win. Out of 5 rounds, You've won ${humanScore} round/s and the computer won ${computerScore} round/s`)
+    }
+    else if (humanScore < computerScore) {
+        console.log(`You lost. Out of 5 rounds, You've won ${humanScore} round/s and the computer won ${computerScore} round/s`)
+    }
+    else {
+        console.log("It's a draw.")
     }
 }
 
-playRound(humanChoice, computerChoice)
-// console.log(humanScore)
-// console.log(computerScore)
+playGame();
