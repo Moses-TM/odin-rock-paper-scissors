4efa89f07381b2134abb25dabd2e4a9aa18d960a
Moses-TM <mosesmagar97@gmail.com>
2025-06-30 04:55:03 +0000

    Refactor game logic to improve output display and user interaction

--- script.js
+++ script.js
@@ -2,96 +2,101 @@ function getComputerChoice() {
     let choiceNumber = Math.floor(Math.random() * 3) + 1
     return (choiceNumber === 1) ? "rock" :
         (choiceNumber === 2) ? "paper" :
-        "scissors"
+            "scissors"
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock Paper Scissors")
-    return humanChoice.toLowerCase()
-}
+function playRound(humanChoice, computerChoice) {
 
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("Draw")
+                result.textContent = "Draw"
+                break
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+            case "paper":
+                console.log("Paper beats Rock. You've lost.")
+                result.textContent = "Paper beats Rock. You've lost."
+                computerScore += 1
+                break
 
-    function playRound(humanChoice, computerChoice) {
-
-        if (humanChoice === "rock") {
-
-            switch (computerChoice) {
-                case "rock":
-                    console.log("Draw")
-                    break
-
-                case "paper":
-                    console.log("Paper beats Rock. You've lost.")
-                    computerScore += 1
-                    break
-
-                case "scissors":
-                    console.log("Rock beats Scissors. You've won.")
-                    humanScore += 1
-                    break
-            }
-        }
-
-        else if (humanChoice === "paper") {
-            switch (computerChoice) {
-                case "rock":
-                    console.log("Paper beats Rock. You've won.")
-                    humanScore += 1
-                    break
-
-                case "paper":
-                    console.log("Draw")
-                    break
-
-                case "scissors":
-                    console.log("Scissors beats Paper. You've lost.")
-                    computerScore += 1
-                    break
-            }
-        }
-
-        else {
-            switch (computerChoice) {
-                case "rock":
-                    console.log("Rock beats Scissors. You've lost.")
-                    computerScore += 1
-                    break
-
-                case "paper":
-                    console.log("Scissors beats Paper. You've won.")
-                    humanScore += 1
-                    break
-
-                case "scissors":
-                    console.log("Draw")
-                    break
-            }
+            case "scissors":
+                console.log("Rock beats Scissors. You've won.")
+                result.textContent = "Rock beats Scissors. You've won."
+                humanScore += 1
+                break
         }
     }
 
-    for (let i = 1; i <= 5; i++) {
-        let humanChoice = getHumanChoice()
-        let computerChoice = getComputerChoice()
-        console.log(`ROUND ${i}:`)
-        console.log(`You've chosen ${humanChoice}`)
-        console.log(`The computer chose ${computerChoice}`)
+    else if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("Paper beats Rock. You've won.")
+                result.textContent = "Paper beats Rock. You've won."
+                humanScore += 1
+                break
 
-        playRound(humanChoice, computerChoice)
+            case "paper":
+                console.log("Draw")
+                result.textContent = "Draw"
+                break
+
+            case "scissors":
+                console.log("Scissors beats Paper. You've lost.")
+                result.textContent = "Scissors beats Paper. You've lost."
+                computerScore += 1
+                break
+        }
     }
 
-    if (humanScore > computerScore) {
-        console.log(`You've won. Out of 5 rounds, You've won ${humanScore} round/s and the computer won ${computerScore} round/s.`)
-    }
-    else if (humanScore < computerScore) {
-        console.log(`You've lost. Out of 5 rounds, You've won ${humanScore} round/s and the computer won ${computerScore} round/s.`)
-    }
     else {
-        console.log(`It's a draw. You've won ${humanScore} round/s and the computer won ${computerScore} round/s as well.`)
+        switch (computerChoice) {
+            case "rock":
+                console.log("Rock beats Scissors. You've lost.")
+                result.textContent = "Rock beats Scissors. You've lost."
+                computerScore += 1
+                break
+
+            case "paper":
+                console.log("Scissors beats Paper. You've won.")
+                result.textContent = "Scissors beats Paper. You've won."
+                humanScore += 1
+                break
+
+            case "scissors":
+                console.log("Draw")
+                result.textContent = "Draw"
+                break
+        }
+    }
+
+    score.innerHTML = "<b> Player Score </b>: " + `${humanScore}` + "<b> Computer Score </b>: " + `${computerScore}`
+
+    if (humanScore === 5) {
+        result.textContent = "Congratulations!! You've won."
+        humanScore = 0
+        computerScore = 0
+    }
+    else if (computerScore === 5) {
+        result.textContent = "You've lost. Better luck next time."
+        humanScore = 0
+        computerScore = 0
     }
 }
 
-playGame();
+let humanScore = 0
+let computerScore = 0
+
+const result = document.querySelector('#result')
+const score = document.querySelector('#score')
+const btn = document.querySelectorAll('button')
+
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        let humanChoice = button.textContent.toLowerCase()
+        let computerChoice = getComputerChoice()
+        console.log(humanChoice)
+        console.log(computerChoice)
+        playRound(humanChoice, computerChoice)
+    })
+})
